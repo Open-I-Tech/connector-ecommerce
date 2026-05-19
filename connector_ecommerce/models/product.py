65d8cca472ac4c2213fb5d614adc0aabf3eb49fd
Romain Deheele <romaindeheele@mac-2.home>
2026-05-19 12:26:18 +0200

    [UPD] Retirer les ensure_one() des computes ou boucler proprement sur self

--- connector_ecommerce/models/product.py
+++ connector_ecommerce/models/product.py
@@ -11,9 +11,9 @@ class ProductTemplate(models.Model):
     # TODO implement set function and also support multi tax
     @api.depends("taxes_id.tax_group_id")
     def _compute_tax_group_id(self):
-        self.ensure_one()
-        taxes = self.taxes_id
-        self.tax_group_id = taxes[-1:].tax_group_id.id
+        for template in self:
+            taxes = template.taxes_id
+            template.tax_group_id = taxes[-1:].tax_group_id.id
 
     tax_group_id = fields.Many2one(
         comodel_name="account.tax.group",
